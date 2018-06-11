81e97e78a8966cf2ca38cf87e2a4b3d24ff5c0e0
ArceusMatt <arceusthegod303@yahoo.com>
2018-06-11 11:29:56 -0400

    Create Main.php

---
+++ src/WorldBorder/Main.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WorldBorder;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\{Utils, Config, TextFormat};
+use pocketmine\command\{Command, CommandSender, CommandExecuter, CommandMap, ConsoleCommandSender};
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerMoveEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->getLogger()->info("\n\nWorldBorder by ArceusMatt\n\n");
+}
+
+public function onMove(PlayerMoveEvent $event){
+$x = $this->getConfig()->get("x");
+$z = $this->getConfig()->get("z");
+$message = $this->getConfig()->get("message");
+/* player's current XZ */
+$xp = $event->getPlayer()->getFloorX();
+$zp = $event->getPlayer()->getFloorZ();
+/* default spawn's XZ */
+$xs = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorX() +x;
+$zs = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorZ() +z;
+/* the magic */
+$x1 = abs($xp);
+$z1= abs($zp);
+$x2 = abs($xs);
+$z2= abs($zs);
+/* checking if player XZ is greater than spawn XZ+config XZ */
+if($x1 >= $x2){
+$event->getPlayer()->sendMessage($message);
+$event->setCancelled();
+}
+if($z1 >= $z2){
+$event->getPlayer()->sendMessage($message);
+$event->setCancelled();
+}
+}
+
+}
